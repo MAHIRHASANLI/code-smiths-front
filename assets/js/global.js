d86fb9517cb4e32bd55a09f3c5dee4f1a45eaa6a
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2024-12-07 13:30:03 +0400

    .

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,17 +2,16 @@ const openNavMenu = document.querySelector(".mobile-menu");
 const mobileLinks = document.querySelector(".mobile-link")
 const closeNavMenu = document.querySelector(".close-menu")
 
-
-openNavMenu.addEventListener("click", openNavMenuHandle);
-
 function openNavMenuHandle() {
     mobileLinks.style.right = "0";
     mobileLinks.style.width = "100%";
 }
 
-closeNavMenu.addEventListener("click", closeMenuHandle);
+openNavMenu.addEventListener("click", openNavMenuHandle);
+
 
 function closeMenuHandle() {
     mobileLinks.style.right = "-800px";
     mobileLinks.style.width = "70%";
 }
+closeNavMenu.addEventListener("click", closeMenuHandle);
